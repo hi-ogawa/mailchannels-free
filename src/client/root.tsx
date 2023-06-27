a8b26c34836e84b6ab776560e798f59ea69b4951
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-06-27 13:19:55 +0900

    chore: tweak style

--- src/client/root.tsx
+++ src/client/root.tsx
@@ -19,12 +19,12 @@ export function Root() {
 function RootInner() {
   return (
     <div className="flex flex-col">
-      <header className="flex items-center p-2 px-4 gap-4 shadow-md shadow-black/[0.05] dark:shadow-black/[0.7] z-1">
+      <header className="sticky top-0 antd-body flex items-center p-2 px-4 gap-4 shadow-md shadow-black/[0.05] dark:shadow-black/[0.7] z-1">
         <div>Mailchannels Demo</div>
         <div className="flex-1"></div>
         <a
           className="antd-btn antd-btn-ghost i-ri-github-line w-5 h-5"
-          href="https://github.com/hi-ogawa/vite-server-build-example"
+          href="https://github.com/hi-ogawa/mailchannels-free"
           target="_blank"
         ></a>
       </header>
@@ -51,7 +51,7 @@ function Content() {
     mutationFn: trpcClient.sendMail.mutate,
     onSuccess(data, _variables, _context) {
       if (data.ok) {
-        toast.error("Successfully send an email");
+        toast.success("Successfully send an email");
       } else {
         toast.error("Failed to send an email");
       }
