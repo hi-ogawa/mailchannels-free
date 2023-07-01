92966ce4ae89c8eb15e9e9ee331518ab7508ddb0
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-07-01 12:34:51 +0900

    refactor: replace trpc with tiny-rpc (#3)

--- src/trpc/context.ts
+++
@@ -1,9 +0,0 @@
-import type { FetchCreateContextFnOptions } from "@trpc/server/adapters/fetch";
-
-export type TrpcAppContext = Awaited<ReturnType<typeof createTrpcAppContext>>;
-
-export const createTrpcAppContext = async (
-  options: FetchCreateContextFnOptions
-) => {
-  return options;
-};
